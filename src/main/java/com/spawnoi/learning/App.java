d75cae248f703d18c2ef4ad633fe890a89816e7e
Josep IA <josep.itarte@gmail.com>
2013-02-22 20:28:44 +0100

    some changes to test.

--- src/main/java/com/spawnoi/learning/App.java
+++ src/main/java/com/spawnoi/learning/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello GitHub! ! ! !" );
+        System.out.println("Just a test app...");
     }
 }
